3d7e3935af5e46fecdbefbc68fd4d47d3a1709af
Benoit <ndiayeben89@gmail.com>
2021-06-14 20:13:20 +0200

    reorganisation des components

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './homepage.styles.scss';
+import Directory from '../components/directory/directory'
 
 export const HomePage = () => {
     return (
         <div className="homepage">
-            <div className="directory-menu">
-                <div className="menu-item">
+            <Directory />
+            
+                {/* <div className="menu-item">
                     <div className="content">
                         <h1 className="title">VESTES</h1>
                         <span className="subtitle">VOIR LES ARTICLES</span>
@@ -35,7 +37,8 @@ export const HomePage = () => {
                         <span className="subtitle">VOIR LES ARTICLES</span>
                     </div>
                 </div>
-            </div>
+            </div> */}
+
             
         </div>
     )
